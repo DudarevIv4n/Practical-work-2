7bdff8e8211828b05c42cd79284a470e34f5a6e0
KAT KAT <kass.kiss@yandex.ru>
2021-10-30 17:54:17 +0300

    Добавьте файлы проекта.

---
+++ LIb_4/Solution.cs
@@ -0,0 +1,26 @@
+﻿using LibMas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIb_4
+{
+    public static class Solution
+    {
+        public static string GetSquareRoot(this MyArray myarray)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < myarray.Length; i++)
+            {
+                if (myarray[i] > 0)
+                {
+                    result += $"{myarray[i]} - " + Math.Round(Math.Sqrt(Convert.ToDouble(myarray[i])), 2) + "\n";
+                }
+            }
+            return result != string.Empty ? result : "Нет чисел > 0!";
+        }
+    }
+}
